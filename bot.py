153e33fc43a7a7e4c6c1cc82d54fa948e576a4e8
nasirma1 <38491634+nasirma1@users.noreply.github.com>
2018-06-04 14:23:31 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import os 
+
+TOKEN = os.environ.get('TOKEN')
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print("bot is ready")
+
+@client.command()
+async def ping():
+    await client.say('pong :ping_pong:')
+
+
+
+@client.command()
+async def staff():
+    embed = discord.Embed(
+      title ='Omega eSports Staff',
+      description = 'Here is Omega eSports Staff',
+      colour = discord.Colour.green()
+    )
+
+    embed.set_footer(text = 'Bot developed by maaz#2031')
+    embed.add_field(name='Leader & Founder', value='AAsuitedAA', inline=False)
+    embed.add_field(name= 'Administrator & Team Manager', value='Megha', inline=False)
+    embed.add_field(name='Tournament Manager', value='Mr_Emix', inline=False)
+    embed.add_field(name='International Clan Director', value='AhmedDiaa', inline=False)
+    embed.add_field(name='CR Clans Director', value='Vianini', inline=False)
+    embed.add_field(name='Comunication Directors', value='Alex \nAmicoMariom', inline=False)
+    embed.add_field(name='Web Master', value='Matteo', inline=False)
+    embed.add_field(name='Graphic designers', value='Matteo \nFlash', inline=False)
+    embed.add_field(name='Social Managers', value='Matteo \nFeib')
+    embed.add_field(name='Discord Manager', value='Maaz', inline=False)
+
+    await client.say(embed=embed)
+
+client.run(TOKEN)
