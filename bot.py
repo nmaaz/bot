f0b82c50a7ef75e99e2af0be0c736cfd64e974d6
nmaaz <39578691+nmaaz@users.noreply.github.com>
2018-06-18 17:20:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
 	if member.server.id=="439171643349139496":
-		msg= member.mention+', Welcome to **Modern Gaming League** \n\nFirst of all please read our **rules** in <#439172562371739698> \n\nLook at <#439172731611643904> for more info about Modern Gaming League and important news \n\nYou want to submit a project , want to sponsor us or ask questions about our organisation ?**Contact the admins!** \n\nFollow us! \nwww.twitter.com/GamModern \nEnjoy your time here!'
+		msg= 'welcome '+member.mention+' to **Modern Gaming League** \n\nFirst of all please read our **rules** in <#439172562371739698> \n\nLook at <#439172731611643904> for more info about Modern Gaming League and important news \n\nYou want to submit a project , want to sponsor us or ask questions about our organisation ?**Contact the admins!** \n\nFollow us! \nwww.twitter.com/GamModern \nEnjoy your time here!'
 		await client.send_message(client.get_channel("439171643349139498"), msg)
 @client.command()
 async def ping():
