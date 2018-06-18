e39c92d0f67a8e2e5dc1c6f5d631aa738cc5b35c
nmaaz <39578691+nmaaz@users.noreply.github.com>
2018-06-18 16:08:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os 
 
 client=commands.Bot(command_prefix='.')
-client.remove_command(help)
+client.remove_command('help')
 @client.event
 async def on_ready():
 	print('Bot is Online')
